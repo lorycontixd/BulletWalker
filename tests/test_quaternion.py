718144cddd47f0cd47216f87ccaddddb3f4768f6
Lorenzo Conti <lorenzo.conti@iit.it>
2024-07-22 11:45:48 +0200

    Updated quaternions test for new (x,y,z,w) notation

--- tests/test_quaternion.py
+++ tests/test_quaternion.py
@@ -7,10 +7,10 @@ class QuaternionTests(unittest.TestCase):
     def test_creation(self):
         # Uses (w, x, y, z) notation
         q = Quaternion(1, 2, 3, 4)
-        self.assertEqual(q.w, 1)
-        self.assertEqual(q.x, 2)
-        self.assertEqual(q.y, 3)
-        self.assertEqual(q.z, 4)
+        self.assertEqual(q.x, 1)
+        self.assertEqual(q.y, 2)
+        self.assertEqual(q.z, 3)
+        self.assertEqual(q.w, 4)
 
     def test_identity(self):
         q = Quaternion.Identity()
@@ -23,10 +23,10 @@ class QuaternionTests(unittest.TestCase):
         q1 = Quaternion(1, 2, 3, 4)
         q2 = Quaternion(5, 6, 7, 8)
         q3 = q1 + q2
-        self.assertEqual(q3.w, 6)
-        self.assertEqual(q3.x, 8)
-        self.assertEqual(q3.y, 10)
-        self.assertEqual(q3.z, 12)
+        self.assertEqual(q3.x, 6)
+        self.assertEqual(q3.y, 8)
+        self.assertEqual(q3.z, 10)
+        self.assertEqual(q3.w, 12)
 
     def test_subtraction(self):
         q1 = Quaternion(1, 2, 3, 4)
@@ -42,37 +42,42 @@ class QuaternionTests(unittest.TestCase):
         q1 = Quaternion(1, 2, 3, 4)
         q2 = Quaternion(5, 6, 7, 8)
         q3 = q1 * q2
-        self.assertEqual(q3.w, -60)
-        self.assertEqual(q3.x, 12)
-        self.assertEqual(q3.y, 30)
-        self.assertEqual(q3.z, 24)
+        self.assertEqual(q3.x, 24)
+        self.assertEqual(q3.y, 48)
+        self.assertEqual(q3.z, 48)
+        self.assertEqual(q3.w, -6)
 
         q1 = Quaternion(1, 1, 1, 1)
-        q2 = Quaternion(1, -1, -1, -1)
+        q2 = Quaternion(-1, -1, -1, 1)
         q3 = q1 * q2
-        self.assertEqual(q3.w, 4)
         self.assertEqual(q3.x, 0)
         self.assertEqual(q3.y, 0)
         self.assertEqual(q3.z, 0)
+        self.assertEqual(q3.w, 4)
 
         # Rotation of 90 degrees about the x-axis
-        q1 = Quaternion(math.sqrt(2) / 2, math.sqrt(2) / 2, 0.0, 0.0)
+        q1 = Quaternion(math.sqrt(2) / 2, 0.0, 0.0, math.sqrt(2) / 2)
         v = [0, 1, 0]
         qv = q1 * v
         self.assertAlmostEqual(qv[0], 0.0)
-        self.assertAlmostEqual(qv[1], 0.0)
+        self.assertAlmostEqual(qv[0], 0.0)
         self.assertAlmostEqual(qv[2], 1.0)
 
-        q1 = Quaternion(math.sqrt(2) / 2, 0.0, math.sqrt(2) / 2, 0.0)
+        # Rotation of 90 degrees about the y-axis
+        q1 = Quaternion(0.0, math.sqrt(2) / 2, 0.0, math.sqrt(2) / 2)
         v = [1, 0, 0]
         qv = q1 * v
         self.assertAlmostEqual(qv[0], 0.0)
         self.assertAlmostEqual(qv[1], 0.0)
         self.assertAlmostEqual(qv[2], -1.0)
 
-        q1 = Quaternion(math.sqrt(2) / 2, 0.0, 0.0, math.sqrt(2) / 2)
+        q1 = Quaternion(0.0, 0.0, math.sqrt(2) / 2, math.sqrt(2) / 2)
         v = [1, 0, 0]
         qv = q1 * v
         self.assertAlmostEqual(qv[0], 0.0)
         self.assertAlmostEqual(qv[1], 1.0)
         self.assertAlmostEqual(qv[2], 0.0)
+
+
+if __name__ == "__main__":
+    unittest.main()
